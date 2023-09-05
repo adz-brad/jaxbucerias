d7b41a9f5263f9c87d1dded942f0f03be1e262ce
Brad S <94940930+adz-brad@users.noreply.github.com>
2023-09-05 11:18:22 -0600

    Update events.js

--- src/data/events.js
+++ src/data/events.js
@@ -1,21 +1,21 @@
 export const bands = [
-    {
-        title: "Retro Beat",
-        day: "Tuesday",
-        time: "8:00 PM",
-        description: "",
-        image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1667406347/Jax%20Bucerias/retro-beat_iulpzo.jpg"
-    },
-    {
-        title: "ALIEN Project",
-        day: "Wednesday",
-        time: "8:00 PM",
-        description: "",
-        image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1686189133/Jax%20Bucerias/alien-project_pao6tl.jpg"
-    },
+    //{
+    //    title: "Retro Beat",
+    //    day: "Tuesday",
+    //    time: "8:00 PM",
+    //   description: "",
+    //   image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1667406347/Jax%20Bucerias/retro-beat_iulpzo.jpg"
+    //},
+    //{
+    //    title: "ALIEN Project",
+    //    day: "Wednesday",
+    //    time: "8:00 PM",
+    //    description: "",
+    //    image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1686189133/Jax%20Bucerias/alien-project_pao6tl.jpg"
+    //},
         {
-        title: "Chris & Jedda",
-        day: "Friday",
+        title: "Two Brothers - Chris & Jedda",
+        day: "Thursday",
         time: "5:00 PM",
        description: "",
        image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1686189134/Jax%20Bucerias/chris-jedda_wnpsmo.jpg"
@@ -27,13 +27,13 @@ export const bands = [
         description: "",
         image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1667406347/Jax%20Bucerias/three-circus_d1iaxs.jpg"
     },
-//    {
-//        title: "THe Availables",
-//        day: "Friday",
-//        time: "5:00 PM",
-//        description: "",
-//        image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1667406347/Jax%20Bucerias/the-availables_zejbgq.jpg"
-//    },
+    {
+        title: "The Availables",
+        day: "Friday",
+        time: "5:00 PM",
+        description: "",
+        image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1667406347/Jax%20Bucerias/the-availables_zejbgq.jpg"
+    },
     {
         title: "The IVM",
         day: "Friday",
