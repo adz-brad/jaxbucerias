274c83fcff566b6238535c3f3b0ffb9df67102da
Brad S <brad@obsidn.io>
2025-11-09 08:53:35 -0700

    update

--- src/data/events.js
+++ src/data/events.js
@@ -1,39 +1,95 @@
 export const bands = [
-    //{
-    //    title: "Los Wendigos",
-    //    day: "Wednesday",
-    //    time: "9:00 PM",
-    //   description: "",
-    //    image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1737493923/Jax%20Bucerias/Los_Wendigos_oteq9f.png"
-    //},
-    //{
-    //    title: "The Osamas",
-    //    day: "Thursday",
-    //    time: "9:00 PM",
-    //    description: "",
-    //    image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1737493946/Jax%20Bucerias/Osamas_cgcwc2.png"
-    //},
-    //{
-    //    title: "The Availables",
-    //    day: "Friday",
-    //    time: "5:00 PM",
-    //    description: "",
-    //    image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1737494034/Jax%20Bucerias/Availables_kqis4v.png"
-    //},
-    //{
-    //    title: "The IVM",
-    //    day: "Friday",
-    //    time: "9:00 PM",
-    //    description: "",
-    //    image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1737494034/Jax%20Bucerias/IVM_af4bln.png"
-    //},
-    //{
-    //    title: "Outline Jam",
-    //    day: "Saturday",
-    //    time: "9:00 PM",
-    //    description: "",
-    //    image: ""
-    //},
+    {
+        title: "Group Therapy",
+        day: "Monday",
+        time: "5:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "Outline Jam",
+        day: "Monday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The B34ST",
+        day: "Tuesday",
+        time: "5:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "Calavera Beach",
+        day: "Tuesday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The Heels",
+        day: "Wednesday",
+        time: "5:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The Animals",
+        day: "Wednesday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "Velvet Storm",
+        day: "Thursday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The Availables",
+        day: "Friday",
+        time: "5:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The IVM & Ximena Esparza",
+        day: "Friday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "Rock of Ages",
+        day: "Saturday",
+        time: "5:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The B34ST",
+        day: "Saturday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "The Gecko Band",
+        day: "Sunday",
+        time: "5:00 PM",
+        description: "",
+        image: ""
+    },
+    {
+        title: "Retro Beat",
+        day: "Sunday",
+        time: "9:00 PM",
+        description: "",
+        image: ""
+    },
 ]
 
 export const events = [
@@ -42,7 +98,7 @@ export const events = [
         day: "Thursday",
         time: "5:00 PM",
         //description: "Trivia is on PAUSE for the summer - study hard and we will see everyone in the fall for Trivia Night at Jax!",
-        description: "Gather the gang and get competitive 5:00PM Thursdays (every other week during summer) at Jaxs! Form a team of 6 or less and get ready to battle it out for trivia supremacy and a free round of drinks for the winners!",
+        description: "Gather the gang and get competitive 5:00PM Thursdays at Jaxs! Form a team of 6 or less and get ready to battle it out for trivia supremacy and a free round of drinks for the winners!",
         image: "https://res.cloudinary.com/de6ty8wwf/image/upload/v1737494323/Jax%20Bucerias/Trivia_gjxymr.png"
     },
 ]
